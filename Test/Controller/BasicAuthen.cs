f2f17d95b10165ddbae1a7df87c99438b2e7d75d
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-04-21 16:55:46 +0700

    authentication

---
+++ Test/Controller/BasicAuthen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test
+{
+   
+    public class BasicAuthenController : ApiController
+    {
+        [BasicAuthentication]
+        public string Get()
+        {
+            return "WebAPI Method Called";
+        }
+    }
+}
+
+/*
+$.ajax({
+    type: 'GET',
+    url: "api/BasicAuthen",
+    datatype: 'json',
+    headers: { Authorization: 'Basic ' + btoa('admin:12345') },
+    success: function (data) { console.log(data); },
+    error: function (data) {}
+});
+ */
